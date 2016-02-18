8f7aac5bb1c53d018a66397c4472804ebb811ee7
sande <sande@172.18.151.130>
2016-02-18 10:57:12 +0100

    SwitchModel en classes voorzien

---
+++ src/models/Model.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+
+/**
+ *
+ * @author sande
+ */
+public class Model implements Observable{
+    
+   private List<InvalidationListener> listenerList = new ArrayList<>();
+
+     protected void fireInvalidationEvent() {
+        for (InvalidationListener listener : listenerList) {
+            listener.invalidated(this);
+        }
+    }
+
+    @Override
+    public void addListener(InvalidationListener listener) {
+         listenerList.add(listener);
+    }
+
+    @Override
+    public void removeListener(InvalidationListener listener) {
+       listenerList.remove(listener);
+    }
+
+  
+    
+    
+    
+}
---
+++ src/models/SwitchModel.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+
+
+/**
+ *
+ * @author sande
+ * 
+ * model voor alle switches in de sidePanels
+ */
+public class SwitchModel extends Model {
+    
+    private void switchOil(){
+       throw new UnsupportedOperationException();
+    }
+    
+    private void switchTires(){
+       throw new UnsupportedOperationException();
+    }
+    
+    private void switchLights(){
+       throw new UnsupportedOperationException();
+    }
+    
+    private void switchRoundAbout(){
+       throw new UnsupportedOperationException();
+    }
+    
+    private void switchHighWay(){
+       throw new UnsupportedOperationException();
+    }
+    
+    private void switchCityTraffic(){
+       throw new UnsupportedOperationException();
+    }
+    
+    private void switchDoubleLaneRoad(){
+       throw new UnsupportedOperationException();
+    }
+    
+    private void switchGas(){
+       throw new UnsupportedOperationException();
+    }
+    
+    private void switchGps(){
+       throw new UnsupportedOperationException();
+    }
+    
+    private void switchEmergencyStop(){
+       throw new UnsupportedOperationException();
+    }
+
+
+    
+}
