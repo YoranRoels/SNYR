5f542f19543fbc5ab7eafeb37c0973e015f30695
Yoran <Yoran@YoranLaptop>
2016-02-18 08:49:05 +0100

    Feature2: User class aangemaakt.

---
+++ src/domein/User.java
@@ -0,0 +1,53 @@
+package domein;
+
+/**
+ *
+ * @author Yoran
+ */
+public class User 
+{
+    String voornaam;
+    String achternaam;
+    String email;
+    String fotoURL;
+    
+    public User(String voornaam, String achternaam, String email, String fotoURL)
+    {
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+        this.email = email;
+        this.fotoURL = fotoURL;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFotoURL() {
+        return fotoURL;
+    }
+
+    public void setFotoURL(String fotoURL) {
+        this.fotoURL = fotoURL;
+    }
+}
