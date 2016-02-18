8b04590040e8c8762e2af4b56a947e495407c98a
sande <sande@172.18.151.130>
2016-02-18 08:46:18 +0100

    package management

--- src/main/RijschoolEva.java
+++ src/main/RijschoolEva.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rijschooleva;
+package main;
 
+import controllers.HomeController;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
