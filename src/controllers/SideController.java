04961cc585ee1348b749173d77d225d2f2a24d0b
sande <sande@192.168.1.3>
2016-02-19 11:40:40 +0100

    Anchorside class en controller added

---
+++ src/controllers/SideController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+/**
+ *
+ * @author sande
+ */
+public class SideController {
+    
+    
+    public void initialize(){
+        System.out.println("sidecontroller");
+    }
+}
---
+++ src/panels/AnchorSide.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package panels;
+
+import controllers.WheelController;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author sande
+ */
+public class AnchorSide extends AnchorPane{
+    
+    public void create(){
+        
+     try{
+            FXMLLoader loader = new FXMLLoader(AnchorWheel.class.getResource("SidePane.fxml"));
+            loader.setRoot(this);
+            loader.setController(new WheelController());
+            loader.load();
+        }
+        catch(IOException ex){
+            throw new RuntimeException(ex);
+        }
+
+    }
+    
+}
