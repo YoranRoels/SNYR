34b099f5baa82256f0f6d648bad98e54011d1a1f
Yoran <Yoran@YoranLaptop>
2016-03-01 13:06:48 +0100

    Feature11A: Added buttons for 2 extra panes (drive & traffic)

--- src/controllers/WheelController.java
+++ src/controllers/WheelController.java
@@ -5,15 +5,30 @@
  */
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 /**
  *
  * @author sande
  */
 public class WheelController {
     
+    @FXML
+    private Button driveButton;
+    @FXML
+    private Button trafficButton;
     
     public void initialize(){
         System.out.println("wheelcontroller");
+        
+        driveButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                driveButton.setText("It works!");
+            }
+        });
     }
-    
 }
