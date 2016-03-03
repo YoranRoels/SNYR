55c916f9e92245ab417a492d5c74c4a2147bddc8
sande <sande@172.18.151.130>
2016-03-03 10:34:25 +0100

    listview zet comments in veld

--- src/controllers/AttitudeController.java
+++ src/controllers/AttitudeController.java
@@ -1,10 +1,13 @@
 package controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 
@@ -21,17 +24,26 @@ public class AttitudeController
     private ListView<String> selectieListView;
     
     @FXML
-    private TextField opmerkingenVeld;
+    private TextArea opmerkingenVeld;
     
     private final BorderPane root;
     
-    private final ObservableList<String> selectie = FXCollections.observableArrayList("Zenuwachtig","Concentractie");
+    private final ObservableList<String> selectie = FXCollections.observableArrayList("Zenuwachtig","Concentractie",
+            "Schrik","Asociaal","Verkeersgevaarlijk","Ongeduldig","Agressief rijgedrag","Inzet","Verstrooid","Eigenwijs");
     
     public void initialize(){
         System.out.println("AttitudeController");
         selectieListView.setItems(selectie);
+        selectieListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                opmerkingenVeld.setText(opmerkingenVeld.getText()+"\n"+newValue);
+                           }
+        });
         //keerTerugButton.setOnMouseClicked(new SwitchPanelCommand(root));
         opmerkingenVeld.setText("test");
+       // opmerkingenVeld.set
     }
 
     public AttitudeController(BorderPane root) 
