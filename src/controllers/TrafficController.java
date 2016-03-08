0fb6f1964053456034a01c3ee407e3bea6c28914
Yoran <Yoran@YoranLaptop>
2016-03-08 17:33:00 +0100

    Quick Overall Changes

--- src/controllers/TrafficController.java
+++ src/controllers/TrafficController.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 
 /**
@@ -12,12 +16,12 @@ public class TrafficController
     private BorderPane root;
     
     public void initialize(){
-        System.out.println("TrafficController"); 
+        System.out.println("DriveController"); 
     }
 
     public TrafficController(BorderPane root) 
     {
-        System.out.println("TrafficController Aangemaakt"); 
+        System.out.println("DriveController Aangemaakt"); 
         this.root = root;
     }
 }
