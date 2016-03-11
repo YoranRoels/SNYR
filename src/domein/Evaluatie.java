d008832f2d5a562469a148b77407e07f08a295f7
Yoran <Yoran@YoranLaptop>
2016-03-11 12:58:26 +0100

    Feature 20: pause

--- src/domein/Evaluatie.java
+++ src/domein/Evaluatie.java
@@ -4,7 +4,19 @@ package domein;
  *
  * @author Yoran
  */
+
 public class Evaluatie 
 {
+    private boolean fueling;
+    private boolean gps;
+    private boolean tires;
+    private boolean emergencystop;
+    private boolean citytraffic;
+    private boolean doublelane;
+    private boolean lights;
+    private boolean oilcheck;
+    private boolean roundabout;
+    private boolean highway;
+    
     
 }
