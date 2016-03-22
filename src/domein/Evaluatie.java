607e6c5e5d8049419a1642848a7f57107858b797
sande <sande@172.18.151.130>
2016-03-22 14:09:50 +0100

    Student voorzien van de nodige klassen om zijn evaluaties bij te houden

--- src/domein/Evaluatie.java
+++ src/domein/Evaluatie.java
@@ -17,6 +17,112 @@ public class Evaluatie
     private boolean oilcheck;
     private boolean roundabout;
     private boolean highway;
+
+    public boolean isFueling() {
+        return fueling;
+    }
+
+    public void setFueling(boolean fueling) {
+        this.fueling = fueling;
+    }
+
+    public boolean isGps() {
+        return gps;
+    }
+
+    public void setGps(boolean gps) {
+        this.gps = gps;
+    }
+
+    public boolean isTires() {
+        return tires;
+    }
+
+    public void setTires(boolean tires) {
+        this.tires = tires;
+    }
+
+    public boolean isEmergencystop() {
+        return emergencystop;
+    }
+
+    public void setEmergencystop(boolean emergencystop) {
+        this.emergencystop = emergencystop;
+    }
+
+    public boolean isCitytraffic() {
+        return citytraffic;
+    }
+
+    public void setCitytraffic(boolean citytraffic) {
+        this.citytraffic = citytraffic;
+    }
+
+    public boolean isDoublelane() {
+        return doublelane;
+    }
+
+    public void setDoublelane(boolean doublelane) {
+        this.doublelane = doublelane;
+    }
+
+    public boolean isLights() {
+        return lights;
+    }
+
+    public void setLights(boolean lights) {
+        this.lights = lights;
+    }
+
+    public boolean isOilcheck() {
+        return oilcheck;
+    }
+
+    public void setOilcheck(boolean oilcheck) {
+        this.oilcheck = oilcheck;
+    }
+
+    public boolean isRoundabout() {
+        return roundabout;
+    }
+
+    public void setRoundabout(boolean roundabout) {
+        this.roundabout = roundabout;
+    }
+
+    public boolean isHighway() {
+        return highway;
+    }
+
+    public void setHighway(boolean highway) {
+        this.highway = highway;
+    }
+
+    public Evaluatie(boolean fueling, boolean gps, boolean tires, boolean emergencystop, boolean citytraffic, boolean doublelane, boolean lights, boolean oilcheck, boolean roundabout, boolean highway) {
+        this.fueling = fueling;
+        this.gps = gps;
+        this.tires = tires;
+        this.emergencystop = emergencystop;
+        this.citytraffic = citytraffic;
+        this.doublelane = doublelane;
+        this.lights = lights;
+        this.oilcheck = oilcheck;
+        this.roundabout = roundabout;
+        this.highway = highway;
+    }
+    
+    public Evaluatie(){
+        this.citytraffic=false;
+        this.doublelane=false;
+        this.emergencystop=false;
+        this.fueling=false;
+        this.gps=false;
+        this.highway=false;
+        this.lights=false;
+        this.oilcheck=false;
+        this.roundabout=false;
+        this.tires=false;
+    }
     
     
 }
--- src/domein/Student.java
+++ src/domein/Student.java
@@ -6,10 +6,24 @@ package domein;
  */
 public class Student 
 {
-    String voornaam;
-    String achternaam;
-    String email;
-    String fotoURL;
+    private String voornaam;
+    private String achternaam;
+    private String email;
+    private String fotoURL;
+    
+    
+    private final static int AANTALEVALUTIES=3;
+    /*arrays die de verschillende evaluaties bijhouden*/
+    private Evaluatie[] evaluatie = new Evaluatie[AANTALEVALUTIES];
+    private Rijtechniek[] rijtechniek = new Rijtechniek[AANTALEVALUTIES];
+    private Verkeerstechniek[]  verkeerstechniek = new Verkeerstechniek[AANTALEVALUTIES];
+    private String[] Attitude = new String[AANTALEVALUTIES];
+    /*level, is de voortgang in de progressbar --> hoe ver staat de student al*/
+    private int[] level = new int[AANTALEVALUTIES];
+    /*hoeveeste evaluatie dit is*/
+    private int evanumber;
+    
+    
     
     public Student(String voornaam, String achternaam, String email, String fotoURL)
     {
@@ -17,6 +31,21 @@ public class Student
         this.achternaam = achternaam;
         this.email = email;
         this.fotoURL = fotoURL;
+        for(int i=0;i<rijtechniek.length;i++){
+            rijtechniek[i]=new Rijtechniek();
+        }
+        for(int i=0;i<evaluatie.length;i++){
+            evaluatie[i]=new Evaluatie();
+        }
+        for(int i=0;i<verkeerstechniek.length;i++){
+            verkeerstechniek[i]=new Verkeerstechniek();
+        }
+        for(int i=0;i<Attitude.length;i++){
+            Attitude[i]="";
+        }
+        for(int i=0;i<level.length;i++){
+            level[i]=0;
+        }
     }
 
     public String getVoornaam() {
