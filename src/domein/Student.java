5093aae525edd865a9f634cae4a59b429f4e8b6d
sande <sande@172.18.151.130>
2016-03-22 14:24:23 +0100

    standaard constructor enzo voor student

--- src/domein/Student.java
+++ src/domein/Student.java
@@ -14,9 +14,9 @@ public class Student
     
     private final static int AANTALEVALUTIES=3;
     /*arrays die de verschillende evaluaties bijhouden*/
-    private Evaluatie[] evaluatie = new Evaluatie[AANTALEVALUTIES];
-    private Rijtechniek[] rijtechniek = new Rijtechniek[AANTALEVALUTIES];
-    private Verkeerstechniek[]  verkeerstechniek = new Verkeerstechniek[AANTALEVALUTIES];
+    private Evaluatie[] evaluaties = new Evaluatie[AANTALEVALUTIES];
+    private Rijtechniek[] rijtechnieks = new Rijtechniek[AANTALEVALUTIES];
+    private Verkeerstechniek[]  verkeerstechnieks = new Verkeerstechniek[AANTALEVALUTIES];
     private String[] Attitude = new String[AANTALEVALUTIES];
     /*level, is de voortgang in de progressbar --> hoe ver staat de student al*/
     private int[] level = new int[AANTALEVALUTIES];
@@ -31,14 +31,14 @@ public class Student
         this.achternaam = achternaam;
         this.email = email;
         this.fotoURL = fotoURL;
-        for(int i=0;i<rijtechniek.length;i++){
-            rijtechniek[i]=new Rijtechniek();
+        for(int i=0;i<rijtechnieks.length;i++){
+            rijtechnieks[i]=new Rijtechniek();
         }
-        for(int i=0;i<evaluatie.length;i++){
-            evaluatie[i]=new Evaluatie();
+        for(int i=0;i<evaluaties.length;i++){
+            evaluaties[i]=new Evaluatie();
         }
-        for(int i=0;i<verkeerstechniek.length;i++){
-            verkeerstechniek[i]=new Verkeerstechniek();
+        for(int i=0;i<verkeerstechnieks.length;i++){
+            verkeerstechnieks[i]=new Verkeerstechniek();
         }
         for(int i=0;i<Attitude.length;i++){
             Attitude[i]="";
@@ -47,6 +47,18 @@ public class Student
             level[i]=0;
         }
     }
+
+    public Student(String voornaam, String achternaam, String email, String fotoURL, int evanumber, Evaluatie[] evaluaties,Rijtechniek[] rijtechnieks,Verkeerstechniek[] verkeerstechnieks) {
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+        this.email = email;
+        this.fotoURL = fotoURL;
+        this.evanumber = evanumber;
+        this.verkeerstechnieks=verkeerstechnieks;
+        this.rijtechnieks=rijtechnieks;
+        this.evaluaties=evaluaties;
+    }
+    
 
     public String getVoornaam() {
         return voornaam;
@@ -79,4 +91,54 @@ public class Student
     public void setFotoURL(String fotoURL) {
         this.fotoURL = fotoURL;
     }
+
+    public Evaluatie[] getEvaluaties() {
+        return evaluaties;
+    }
+
+    public void setEvaluaties(Evaluatie[] evaluatie) {
+        this.evaluaties = evaluatie;
+    }
+
+    public Rijtechniek[] getRijtechnieks() {
+        return rijtechnieks;
+    }
+
+    public void setRijtechnieks(Rijtechniek[] rijtechniek) {
+        this.rijtechnieks = rijtechniek;
+    }
+
+    public Verkeerstechniek[] getVerkeerstechnieks() {
+        return verkeerstechnieks;
+    }
+
+    public void setVerkeerstechnieks(Verkeerstechniek[] verkeerstechniek) {
+        this.verkeerstechnieks = verkeerstechniek;
+    }
+
+    public String[] getAttitude() {
+        return Attitude;
+    }
+
+    public void setAttitude(String[] Attitude) {
+        this.Attitude = Attitude;
+    }
+
+    public int[] getLevel() {
+        return level;
+    }
+
+    public void setLevel(int[] level) {
+        this.level = level;
+    }
+
+    public int getEvanumber() {
+        return evanumber;
+    }
+
+    public void setEvanumber(int evanumber) {
+        this.evanumber = evanumber;
+    }
+    
+    
 }
