5494712d8e8acb2c53a686ba7ae76af221d99911
sande <sande@172.18.151.130>
2016-03-24 10:57:30 +0100

    rijtechniek persisteert

--- src/models/RijModel.java
+++ src/models/RijModel.java
@@ -26,22 +26,76 @@ public class RijModel extends Model {
         kleuren.put("orange", Color.ORANGE);
     }
     
-    public void selecteerTechniek(String punt,String techniek){
+    public void setColorForTechniek(String punt,String techniek){
         switch(techniek){
-            case "clutch": System.out.println("clutch");
-                           System.out.println(kleuren.get(punt));
-                           student.getCurrentRijtechniek().getClutch().setColor(kleuren.get(punt));
+            case "clutch": student.getCurrentRijtechniek().getClutch().setColor(kleuren.get(punt));
                         break;
-            case "braking": System.out.println("braking");
+            case "brake": student.getCurrentRijtechniek().getBraking().setColor(kleuren.get(punt));
                         break;
-            case "steering": System.out.println("steering");
+            case "steering": student.getCurrentRijtechniek().getSteering().setColor(kleuren.get(punt));
+                        break;
+            case "garage": student.getCurrentRijtechniek().getGarage().setColor(kleuren.get(punt));
+                        break;
+            case "hill": student.getCurrentRijtechniek().getHillBalancing().setColor(kleuren.get(punt));
+                        break;
+            case "looking": student.getCurrentRijtechniek().getLooking().setColor(kleuren.get(punt));
+                        break;
+            case "parking": student.getCurrentRijtechniek().getParking().setColor(kleuren.get(punt));
+                        break;
+            case "sitting": student.getCurrentRijtechniek().getPosture().setColor(kleuren.get(punt));
+                        break;
+            case "reverse": student.getCurrentRijtechniek().getReverse().setColor(kleuren.get(punt));
+                        break;
+            case "shifting": student.getCurrentRijtechniek().getShifting().setColor(kleuren.get(punt));
+                        break;
+            case "steeringPractice": student.getCurrentRijtechniek().getSteeringPractice().setColor(kleuren.get(punt));
+                        break;
+            case "turning": student.getCurrentRijtechniek().getTurning().setColor(kleuren.get(punt));
                         break;
         }
+        fireInvalidationEvent();
                 
     }
     
     public Color getClutchColor(){
         return student.getCurrentRijtechniek().getClutch().getColor();
+    }
+    public Color getBrakingColor(){
+        return student.getCurrentRijtechniek().getBraking().getColor();
+    }
+    public Color getGarageColor(){
+        return student.getCurrentRijtechniek().getGarage().getColor();
+    }
+    public Color getHillColor(){
+        return student.getCurrentRijtechniek().getHillBalancing().getColor();
+    }
+    public Color getLookingColor(){
+        return student.getCurrentRijtechniek().getLooking().getColor();
+    }
+    public Color getParkingColor(){
+        return student.getCurrentRijtechniek().getParking().getColor();
+    }
+    public Color getPostureColor(){
+        return student.getCurrentRijtechniek().getPosture().getColor();
+    }
+    public Color getReverseColor(){
+        return student.getCurrentRijtechniek().getReverse().getColor();
+    }
+    public Color getShiftingColor(){
+        return student.getCurrentRijtechniek().getShifting().getColor();
+    }
+    public Color getSteeringColor(){
+        return student.getCurrentRijtechniek().getSteering().getColor();
+    }
+    public Color getSteeringPracticeColor(){
+        return student.getCurrentRijtechniek().getSteeringPractice().getColor();
+    }
+    public Color getTurningColor(){
+        return student.getCurrentRijtechniek().getTurning().getColor();
+    }
+    
+    public void EvaNumberChanged(){
+        fireInvalidationEvent();
     }
     
     
