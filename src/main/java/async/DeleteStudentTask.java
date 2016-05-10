e19e03e6a80be3f904daed5703ce674456dc628a
Yoran <Yoran@YoranLaptop>
2016-05-10 16:38:59 +0200

    Database

---
+++ src/main/java/async/DeleteStudentTask.java
@@ -0,0 +1,47 @@
+package async;
+
+import domein.Student;
+import javafx.concurrent.Task;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import json.StudentWriter;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+
+/**
+ *
+ * @author Yoran
+ */
+public class DeleteStudentTask extends Task<Void> 
+{
+    private final WebTarget studentListResource;
+
+    public DeleteStudentTask(Student student)
+    {
+        studentListResource = ClientBuilder.newClient()
+                .target("http://localhost:8080/rijschool/api/")
+                .path("students")
+                .path(Integer.toString(student.getStudentnr()))
+                .register(HttpAuthenticationFeature.basic("rijschoolevauser", "rijschoolevauser"));
+    }
+    
+    @Override
+    protected Void call() throws Exception {
+        Response response = studentListResource.request().delete();
+        switch (response.getStatus()){
+            case 204:
+                return null;
+            case 401:
+            case 403:
+                throw new RuntimeException();
+            case 404:
+                throw new NotFoundException();
+            default:
+                throw new RuntimeException();
+        }
+    }
+}
---
+++ src/main/java/async/GetStudentListTask.java
@@ -0,0 +1,41 @@
+package async;
+
+import domein.Student;
+import java.util.List;
+import javafx.concurrent.Task;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import json.StudentListReader;
+
+/**
+ *
+ * @author Yoran
+ */
+public class GetStudentListTask extends Task<List<Student>>
+{
+    private final WebTarget userListResource;
+
+    public GetStudentListTask()
+    {
+        userListResource = ClientBuilder.newClient()
+                .target("http://localhost:8080/rijschool/api/")
+                .path("students")
+                .register(StudentListReader.class);
+    }
+    
+    @Override
+    protected List<Student> call() throws Exception {
+        Response response = userListResource.request(MediaType.APPLICATION_JSON).get();
+        if (response.getStatus() == 200)
+        {
+            List<Student> students = response.readEntity(new GenericType<List<Student>>() {});
+            return students;
+        } else
+        {
+            throw new RuntimeException();
+        }
+    }
+}
